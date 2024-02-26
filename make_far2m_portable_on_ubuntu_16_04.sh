b59b03982665a04b1777714bababa3fdcbad570f
Zeroes1 <Zeroes1@gmail.com>
2024-02-27 00:00:33 +0300

    Update make_far2m_portable_on_ubuntu_16_04.sh

--- make_far2m_portable_on_ubuntu_16_04.sh
+++ make_far2m_portable_on_ubuntu_16_04.sh
@@ -15,43 +15,56 @@ rm -rf far2m
 mkdir far2m
 cd far2m
 apt-get update
+
 if [ "$gui_arg_present" = true ]; then
     sudo apt install -y libwxgtk3.0-dev    
     sudo apt install -y libwxgtk3.0-gtk3-dev
     sudo apt install -y libwxgtk3.2-dev
     sudo apt install -y libsmbclient-dev
 fi
-apt-get install -y libneon27-dev
-apt-get install -y libspdlog-dev patchelf wget gawk m4 libx11-dev libxi-dev libxerces-c-dev libuchardet-dev libnss-mdns libssh-dev libssl-dev libnfs-dev libarchive-dev libpcre3-dev cmake g++ git
-# additional
-apt-get install -y libluajit-5.1-dev luajit uuid-dev
+
+apt-get install -y libspdlog-dev patchelf wget gawk m4 libx11-dev libxi-dev libxerces-c-dev libuchardet-dev libssh-dev libssl-dev libnfs-dev libneon27-dev libarchive-dev libpcre3-dev cmake g++ git
+apt-get install -y libluajit-5.1-dev uuid-dev
 git clone https://github.com/shmuz/far2m
 cd far2m
-# wget https://raw.githubusercontent.com/unxed/far2l-deb/master/portable/tty_tweaks.patch
+# if [ -f "/tty_tweaks.patch" ]; then
+#   cp /tty_tweaks.patch .
+# else
+#   wget https://raw.githubusercontent.com/unxed/far2l-deb/master/portable/tty_tweaks.patch
+# fi
 # git apply tty_tweaks.patch
 mkdir build
 cd build
 if [ "$gui_arg_present" = true ]; then
-    cmake -DLEGACY=no -DCMAKE_BUILD_TYPE=Release ..
+    cmake -DUSEWX=yes -DNETCFG=no -DCMAKE_BUILD_TYPE=Release ..
 else
-    cmake -DUSEWX=no -DLEGACY=no -DCMAKE_BUILD_TYPE=Release ..
+    cmake -DUSEWX=no  -DNETCFG=no -DCMAKE_BUILD_TYPE=Release ..
 fi
 make -j$(nproc --all)
 cd install
-rm -rf far2m_askpass
-echo "#!/bin/bash" > far2m_askpass
-echo "cd \"\$FARHOME\"" >> far2m_askpass
-echo "exec -a far2m_askpass ./far2m \$*" >> far2m_askpass
-chmod +x far2m_askpass
-# wget https://github.com/unxed/far2l-deb/raw/master/portable/far2l.sh
-wget https://github.com/Zeroes1/far2m-portable/raw/main/far2m.sh
-chmod +x far2m.sh
-wget https://github.com/unxed/far2l-deb/raw/master/portable/autonomizer.sh
+rm -rf ./lib
+mkdir lib
+# if [ -f "./Plugins/luafar/luafar.so" ]; then
+#   mv ./Plugins/luafar/luafar.so ./lib/luafar.so
+# fi
+if [ -f "/usr/lib/x86_64-linux-gnu/libluajit-5.1.so.2.0.4" ]; then
+  cp /usr/lib/x86_64-linux-gnu/libluajit-5.1.so.2.0.4 ./lib/libluajit-5.1.so
+fi
+if [ -f "/autonomizer.sh" ]; then
+  cp /autonomizer.sh .
+fi
 chmod +x autonomizer.sh
 ./autonomizer.sh
-rm -rf autonomizer.sh
+rm -f autonomizer.sh
+rm lib/libc.so.6
+rm lib/libdl.so.2
+rm lib/libgcc_s.so.1
+rm lib/libm.so.6
+rm lib/libpthread.so.0
+rm lib/libstdc++.so.6
+rm lib/libresolv.so.2
+rm lib/librt.so.1
 cd ..
 mv install far2m_portable
 git clone https://github.com/megastep/makeself.git
-makeself/makeself.sh --keep-umask far2m_portable far2m_portable.run far2m ./far2m
-
+makeself/makeself.sh far2m_portable far2m_portable.run far2m ./far2m
